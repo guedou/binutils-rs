b06e162bdeeac7087fbef5532bda1dd38975c9db
Guillaume Valadon <guillaume@valadon.net>
2018-04-18 14:26:53 +0200

    Use Rust type for the bindings

--- rust/test_binary/src/main.rs
+++ rust/test_binary/src/main.rs
@@ -6,29 +6,18 @@ use libc::{c_char, c_int, c_ulong};
 use std::ffi::{CStr, CString};
 use std::io::Write;
 
+
 // libbfd bindings
-#[link(name = "bfd-2.28-multiarch")]
-extern "C" {
-    fn bfd_init();
 
-    fn bfd_get_error() -> c_int;
-    fn bfd_errmsg(error_tag: c_int) -> *const c_char;
-
-    fn bfd_openr(filename: *const c_char, target: *const c_char) -> *const c_int;
-
-    fn bfd_check_format(bfd: *const c_int, bfd_format: c_int) -> bool;
-
-    fn bfd_get_section_by_name(bfd: *const c_int, name: *const c_char) -> *const c_int;
-
-/*
- * binutils 2.29.1
-    fn bfd_get_arch(bfd: *const c_int) -> c_int;
-    fn bfd_get_mach(bfd: *const c_int) -> c_long;
-*/
-}
+// Rust bfd types
+// Note: - trick from https://doc.rust-lang.org/nomicon/ffi.html
+//       - it allows to use the Rust type checker
+enum Section {}
+enum Bfd {}
 
 #[allow(non_camel_case_types)] // use the same enum names as libbfd
 #[allow(dead_code)] // don't warn that some variants are not used
+#[repr(C)]
 enum BfdFormat {
     bfd_unknown = 0,
     bfd_object,
@@ -37,46 +26,68 @@ enum BfdFormat {
     bfd_type_end,
 }
 
+#[link(name = "bfd-2.28-multiarch")]
+extern "C" {
+    fn bfd_init();
+
+    fn bfd_get_error() -> c_int;
+    fn bfd_errmsg(error_tag: c_int) -> *const c_char;
+
+    fn bfd_openr(filename: *const c_char, target: *const c_char) -> *const Bfd;
+
+    fn bfd_check_format(bfd: *const Bfd, bfd_format: BfdFormat) -> bool;
+
+    fn bfd_get_section_by_name(bfd: *const Bfd, name: *const c_char) -> *const Section;
+
 /*
-#[repr(C)]
-struct BfdSection {
-    name: *const c_char,
-    id: c_uint,
-    index: c_uint,
-}
+ * binutils 2.29.1
+    fn bfd_get_arch(bfd: *const c_int) -> c_int;
+    fn bfd_get_mach(bfd: *const c_int) -> c_long;
 */
+}
+
 
 // libopcodes bindings
+enum DisassembleInfo {}
+
 #[link(name = "opcodes-2.28-multiarch")]
 extern "C" {
-    fn disassembler(bfd: *const c_int)
-        -> extern "C" fn(pc: c_ulong, info: *const c_int) -> c_ulong;
-    //fn disassembler(bfd: *const c_int) -> *const c_int; // TODO: function pointer
+    fn disassembler(
+        bfd: *const Bfd,
+    ) -> extern "C" fn(pc: c_ulong, info: *const DisassembleInfo) -> c_ulong;
     /*
      * binutils 2.29.1
     fn disassembler(arc: c_int, big: bool, mach: c_long, bfd: *const c_int) -> *const c_int;
     */
-    fn disassemble_init_for_target(dinfo: *const c_int);
+    fn disassemble_init_for_target(dinfo: *const DisassembleInfo);
 }
 
+
+// Custom bindings that ease disassembler_info manipulation
 extern "C" {
-    fn new_disassemble_info() -> *const c_int;
-    fn configure_disassemble_info(info: *const c_int, section: *const c_int, bfd: *const c_int);
-    fn get_start_address(bfd: *const c_int) -> c_ulong;
-    fn get_section_size(section: *const c_int) -> c_ulong;
+    fn new_disassemble_info() -> *const DisassembleInfo;
+    fn configure_disassemble_info(
+        info: *const DisassembleInfo,
+        section: *const Section,
+        bfd: *const Bfd,
+    );
+    fn get_start_address(bfd: *const Bfd) -> c_ulong;
+    fn get_section_size(section: *const Section) -> c_ulong;
     fn set_print_address_func(
-        info: *const c_int,
-        print_function: extern "C" fn(c_ulong, *const c_int),
+        info: *const DisassembleInfo,
+        print_function: extern "C" fn(c_ulong, *const DisassembleInfo),
     );
 
     fn flush_stdout();
 }
 
-extern "C" fn override_print_address(addr: c_ulong, _info: *const c_int) {
+
+extern "C" fn override_print_address(addr: c_ulong, _info: *const DisassembleInfo) {
     unsafe { flush_stdout() };
     print!("0x{:x}", addr);
     let _ = std::io::stdout().flush();
 }
+
 
 fn main() {
     let filename = CString::new("/bin/ls").unwrap();
@@ -92,7 +103,7 @@ fn main() {
         return;
     }
 
-    if !unsafe { bfd_check_format(bfd_file, BfdFormat::bfd_object as i32) } {
+    if !unsafe { bfd_check_format(bfd_file, BfdFormat::bfd_object) } {
         let error = unsafe { bfd_get_error() };
         let msg = unsafe { bfd_errmsg(error) };
         println!("Error [{}]: {:?}", error, unsafe { CStr::from_ptr(msg) });
@@ -114,15 +125,19 @@ fn main() {
         return;
     }
 
+    // Make a disassemble_info structure
     let info = unsafe { new_disassemble_info() };
     if info.is_null() {
         println!("Error while getting disassemble_info!");
         return;
     }
+
+    // Configure the disassemble_info structure
     unsafe { configure_disassemble_info(info, section, bfd_file) };
     unsafe { set_print_address_func(info, override_print_address) };
     unsafe { disassemble_init_for_target(info) };
 
+    // Disassemble the binary
     let mut pc = unsafe { get_start_address(bfd_file) };
     let section_size = unsafe { get_section_size(section) };
 
