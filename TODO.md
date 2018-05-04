d6a7b2888285952e966edc58d040451c21410855
Guillaume Valadon <guillaume@valadon.net>
2018-05-04 16:10:48 +0200

    C helpes refactoring

--- TODO.md
+++ TODO.md
@@ -1,28 +1,10 @@
-## Rust
+## Before releasing 0.1.0
 
-[x] newer binutils version
-[x] disassemble a buffer passed from Rust
-[x] build binutils with cargo
-[x] build libraries with all architectures using build.rs
-[x] refactor the crate with 'mod': Bfd, DisassembleInfo, ...
-[x] binutils-rs
-[x] find a way to avoid specifying `LD_LIBRARY_PATH`
-[x] implement the DisassemleInfo destructor
-[x] rename tmp_buf_asm: the ptr is likely useless!
-[x] rename scan_arch to set_arch_mach
-[x] clean and comment functions
-[x] licence MIT
-[ ] try to use the va crate!
+[x] try to use the va crate!
 [ ] move main.rs to examples/
 [ ] check binutils tarball checksum
-[x] build to to OUT_DIR & use static libraries !
-[x] download binutils-2.29.1.tar.gz from build.rs!
-[x] fix cargo package
-[ ] check if the MeP specific helper is useful
+[ ] check if the MeP specific helper is really useful
 [ ] get_opcode: use buffer_asm_ptr to get the NUL byte
-[ ] use the error_chain crate
-
-[x] make disassemble return an Instruction structure
 
 [ ] build a high level API to disassemble a section from an ELF and a buffer
     disassemble_elf_section!("file", ".text")
@@ -30,12 +12,13 @@
 
 [ ] write examples
 [ ] write tests
-[ ] fuzz the disassembler
-[ ] generate documentation from comments?
-
-[ ] use info->stop_vma ?
-[x] make disassemble return an iterator
 
 ## Wish list / Roadmap
-[ ] use features to select architectures
+
 [ ] generate mach.rs with build.rs
+[ ] use features to select architectures
+[ ] use info->stop_vma ?
+[ ] use the error_chain crate
+[ ] generate documentation from comments
+[ ] rewrite copy_buffer in Rust
+[ ] fuzz the disassembler
--- src/helpers.c
+++ src/helpers.c
@@ -14,26 +14,41 @@
 // Silly macro that helps removing the unused warnings
 #define UNUSED_VARIABLE(id) id=id
 
-#define R_ASM_BUFSIZE 64
-char buffer_asm[R_ASM_BUFSIZE];
+
+/*** Generic helpers ***/
+
+#define BUFFER_SIZE 64
+char buffer_asm[BUFFER_SIZE];
 char *buffer_asm_ptr = buffer_asm;
 void copy_buffer(void * useless, const char* format, ...) {
+    /* Construct the final opcode into buffer_asm */
     UNUSED_VARIABLE(useless);
 
-    // TODO: handle this in Rust?
     va_list ap;
 
     va_start(ap, format);
-    vsnprintf(buffer_asm_ptr, R_ASM_BUFSIZE-(buffer_asm_ptr-buffer_asm), format, ap);
+    vsnprintf(buffer_asm_ptr, BUFFER_SIZE-(buffer_asm_ptr-buffer_asm), format, ap);
     buffer_asm_ptr = buffer_asm + strlen(buffer_asm);
     va_end(ap);
 }
 
+void show_buffer(struct disassemble_info *info) {
+    printf("len=%d - vma=%lu\n", info->buffer_length, info->buffer_vma);
+    printf("%p\n", info->buffer);
+    printf("%x\n", info->buffer[0]);
+    printf("%x\n", info->buffer[1]);
+    printf("%x\n", info->buffer[2]);
+    printf("%x\n", info->buffer[3]);
+}
+
+
+/*** disassemble_info structure helpers ***/
+
 struct disassemble_info *new_disassemble_info() {
+    /* Return a new structure */
     struct disassemble_info *info = malloc (sizeof(struct disassemble_info));
     return info;
 }
-
 void configure_disassemble_info(struct disassemble_info *info, asection *section, bfd *bfdFile) {
     /* Construct and configure the disassembler_info class using stdout */
   
@@ -50,6 +65,7 @@ void configure_disassemble_info(struct disassemble_info *info, asection *section
 
 
 void configure_disassemble_info_buffer(struct disassemble_info *info, enum bfd_architecture arch, unsigned long mach) {
+    /* A variant of configure_disassemble_info() for buffers */
   
     init_disassemble_info (info, stdout, (fprintf_ftype) copy_buffer);
     info->arch = arch;
@@ -57,28 +73,13 @@ void configure_disassemble_info_buffer(struct disassemble_info *info, enum bfd_a
     info->read_memory_func = buffer_read_memory;
 }
 
-
-unsigned long get_start_address(bfd *bfdFile) {
-    return bfdFile->start_address;
-}
-
-
-unsigned long get_section_size(asection *section) {
-    return section->size;
-}
-
 typedef void (*print_address_func) (bfd_vma addr, struct disassemble_info *dinfo);
-
 void set_print_address_func(struct disassemble_info *info, print_address_func print_function) {
     info->print_address_func = print_function;
 }
 
-
-unsigned int macro_bfd_big_endian(bfd *bfdFile) {
-    return bfd_big_endian(bfdFile);
-}
-
 void set_buffer(struct disassemble_info *info, bfd_byte* buffer, unsigned int length, bfd_vma vma) {
+    /* Configure the buffet that will be disassembled */
     info->buffer = buffer;
     info->buffer_length = length;
     info->buffer_vma = vma;
@@ -88,30 +89,15 @@ void set_buffer(struct disassemble_info *info, bfd_byte* buffer, unsigned int le
     info->section->vma = vma;
 }
 
+unsigned long get_disassemble_info_section_vma(struct disassemble_info *info) {
+  return info->section->vma;
+}
+
 void free_disassemble_info(struct disassemble_info *info) {
+  /* Free the structure and allocated variable */
   free(info->section);
   free(info);
 }
-
-
-void show_buffer(struct disassemble_info *info) {
-    printf("len=%d - vma=%lu\n", info->buffer_length, info->buffer_vma);
-    printf("%p\n", info->buffer);
-    printf("%x\n", info->buffer[0]);
-    printf("%x\n", info->buffer[1]);
-    printf("%x\n", info->buffer[2]);
-    printf("%x\n", info->buffer[3]);
-}
-
-
-enum bfd_architecture get_arch(struct bfd_arch_info *arch_info) {
-  return arch_info->arch;
-}
-
-unsigned long get_mach(struct bfd_arch_info *arch_info) {
-  return arch_info->mach;
-}
-
 
 void mep_disassemble_info(struct disassemble_info* info) {
   // From GDB
@@ -128,6 +114,30 @@ void mep_disassemble_info(struct disassemble_info* info) {
 }
 
 
-unsigned long get_disassemble_info_section_vma(struct disassemble_info *info) {
-  return info->section->vma;
+/*** bfd structure helpers ***/
+
+unsigned long get_start_address(bfd *bfdFile) {
+    return bfdFile->start_address;
+}
+
+unsigned int macro_bfd_big_endian(bfd *bfdFile) {
+    return bfd_big_endian(bfdFile);
+}
+
+
+/*** bfd_arch_info structure helpers ***/
+
+enum bfd_architecture get_arch(struct bfd_arch_info *arch_info) {
+  return arch_info->arch;
+}
+
+unsigned long get_mach(struct bfd_arch_info *arch_info) {
+  return arch_info->mach;
+}
+
+
+/*** section structure helpers ***/
+
+unsigned long get_section_size(asection *section) {
+    return section->size;
 }
